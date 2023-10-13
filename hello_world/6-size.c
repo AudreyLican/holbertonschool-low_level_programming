06dfd5e0bc60ec3639a4fc2cc656e58ce5b19419
AudreyLican <audrey.lican@gmail.com>
2023-10-13 01:09:27 -0700

    ex 4 to 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Size of a char: %ld\n", sizeof(char));
+	printf("Size of an int: %ld\n", sizeof(int));
+	printf("");
+	printf("");
+	printf("");
+}
