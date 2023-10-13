245e874b69d18eacd647a49a8acb15fa4fb3cc50
AudreyLican <audrey.lican@gmail.com>
2023-10-13 15:44:25 +0200

    ex 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,30 +1,13 @@
 #include <unistd.h>
 
 /**
-* ft_putstr - Entry point
-*
-* str: is string - return string
-*/
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '0')
-	{
-		write(1, str, 1);
-		str++;
-	}
-}
-
-/**
 * main - Entry point
 *
 * Return: Always 1
 */
 int	main(void)
 {
-	ft_putstr("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
