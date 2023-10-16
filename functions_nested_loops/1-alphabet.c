2cc0a12b423a5916224beaa2aa7ee98c9df1f17a
AudreyLican <audrey.lican@gmail.com>
2023-10-16 19:51:36 +0200

    ex 0, 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <_putchar.h>
+#include <main.h>
+
+/**
+* Description: print the alphabet in lowercase
+*/
+void	print_alphabet(void)
+{
+	char	lowcase;
+
+	lowcase = 'a';
+	while (lowercase < 'z')
+	{
+		_putchar(lowcase);
+		lowcase++;
+	}
+	_putchar('\n);
+
+	return (0);
+}
