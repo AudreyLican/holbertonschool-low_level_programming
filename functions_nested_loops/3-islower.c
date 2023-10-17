dc473a08ff5d17fe1cc6414c0e551ca4d239dfce
AudreyLican <audrey.lican@gmail.com>
2023-10-17 15:27:18 +0200

    update betty ex 3, create ex 4

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _islower - check if char given is lowcase or not
-* 
+*
 * @c : parameter is a int character
 *
 * Return: 1 if yes, and 0 for no
---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - for alphabetic characters
+* @c : character input in integer
+*
+* Return: 1 if true and 0 false
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
