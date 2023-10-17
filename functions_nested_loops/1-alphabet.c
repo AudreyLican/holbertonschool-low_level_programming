c1c6bdf226d31c3de26fbde95e164a626ac8d012
AudreyLican <audrey.lican@gmail.com>
2023-10-17 10:49:12 +0200

    update ex 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 
 /**
+* print_alphabet - function print entry point
+*
 * Description: print the alphabet in lowercase
 */
 void	print_alphabet(void)
@@ -10,7 +12,7 @@ void	print_alphabet(void)
 	char	lowercase;
 
 	lowercase = 'a';
-	while (lowercase < 'z')
+	while (lowercase <= 'z')
 	{
 		_putchar(lowercase);
 		lowercase++;
