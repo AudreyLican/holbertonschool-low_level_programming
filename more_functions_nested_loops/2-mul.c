1cf5b1ee358d9fe1b83791e2b2588dc25afda8da
AudreyLican <audrey.lican@gmail.com>
2023-10-21 16:02:50 +0200

    create ex 2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - function that multiplies two integers
+* @a : 1st given integer
+* @b : @nd givin integer
+*
+* Return: multiplication product btw a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -12,4 +12,7 @@ int _isupper(int c);
 /* function that checks for a digit (0 through 9) */
 int _isdigit(int c);
 
+/* function that multiplies two integers */
+int mul(int a, int b);
+
 #endif
