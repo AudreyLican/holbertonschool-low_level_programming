9e7b9b5643e8693527c961eba6c29edd3499320e
AudreyLican <audrey.lican@gmail.com>
2023-10-22 15:05:20 +0200

    update ex 9-fizz_buzz, running

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,27 @@
 */
 int	main(void)
 {
-	int	m3;
-	int	m5;
 	int	i;
 
+	for ( i = 1; i <= 100; ++i)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
