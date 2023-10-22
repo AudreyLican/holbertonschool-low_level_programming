3ec8613c03bdec414089ce5185b433c0628f777b
AudreyLican <audrey.lican@gmail.com>
2023-10-22 20:54:10 +0200

    update betty ex 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,18 @@
 
 /**
 * print_last_digit - print the last digit of a number
+* @x : integer value
 *
-* Return: digit for last number
+* Return: value of nb
 */
 int print_last_digit(int x)
 {
 	int	nb;
 
 	nb = x % 10;
-
 	if (x < 0)
 		nb = nb * -1;
 	_putchar(nb + 48);
 
-	return(nb);
+	return (nb);
 }
