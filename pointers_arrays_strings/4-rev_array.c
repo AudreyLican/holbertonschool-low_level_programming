52682753b80819410982a2864e017bac30a6d04e
AudreyLican <audrey.lican@gmail.com>
2023-10-26 12:31:53 +0200

    update 4-rev_array, comments mistake fixed

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-* reverse_array - function that reverses the content of an array of integers
-* @a: array
-* @n: array size
-*/
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: array
+ * @n: array size
+ */
 
-void	reverse_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
-	int	i = 0;
-	int	j;
-	//char	temp;
+	int i = 0;
+	int j;
 
 	while (i < n / 2)
 	{
