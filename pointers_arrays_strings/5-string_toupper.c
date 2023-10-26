563354f5a03fe12b317ffa65a751c97d4c556784
AudreyLican <audrey.lican@gmail.com>
2023-10-26 12:40:02 +0200

    update 5-string_toupper, loop fixed

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
-* *string_toupper - function that changes all lowercase letters 
-* of a string to uppercase
-* @str: string
-*/
+ * *string_toupper - function that changes all lowercase letters
+ * of a string to uppercase
+ * @str: string
+ *
+ * Return: str value
+ */
 
 char *string_toupper(char *str)
 {
-	int	i = 0;
+	int i = 0;
 
-	while (str[i]  != '\0')
+	while (str[i] != '\0')
 	{
-		if (i > 97 && i < 122)
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] += 32;
+			str[i] -= 32;
 		}
 		i++;
 	}
