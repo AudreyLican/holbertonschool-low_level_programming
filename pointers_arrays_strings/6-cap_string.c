db33984c82cd532212a4d9542776df4b974dfd5f
AudreyLican <audrey.lican@gmail.com>
2023-10-27 15:48:41 +0200

    ex 6-cap_string, running

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,39 @@
 #include "main.h"
 
 /**
-* *cap_string -  function that capitalizes all words of a string
-* @str: string
-*
-* Return: string capitalizes
-*/
+ * _alphanum - function to look for other that letters & numbers
+ * @c : char value
+ *
+ * *cap_string -  function that capitalizes all words of a string
+ * @str : string
+ *
+ * Return: string capitalizes
+ */
+
+int _alphanum(char c)
+{
+	if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (0);
+	return (1);
+}
 
 char *cap_string(char *str)
 {
-	int	i= 0;
+	int i = 0;
+	/*int j = (str[i] >= 'a' && str[i] <= 'z');*/
+	/*int k = (str[i] >= 'A' && str[i] <= 'Z');*/
 
 	while (str[i] != '\0')
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z'))
-			&& (str[i - 1] 
-		{
+		if ((str[i] >= 'A' && str[i] <= 'Z'))
+			str[i] += 32;
+		i++;
+	}
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') && (_alphanum(str[i - 1]) || i == 0))
 			str[i] -= 32;
-		}
 		i++;
 	}
 	return (str);
