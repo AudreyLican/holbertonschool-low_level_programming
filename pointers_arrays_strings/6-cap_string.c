4130eafd92ad429465ce21c8b3b18d78b98b652f
AudreyLican <audrey.lican@gmail.com>
2023-10-27 21:32:08 +0200

    update ex 6, all code

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,17 +6,13 @@
  *
  * Return: 0 if alphanum and 1 false
  */
-/*int _alphanum(char c)
+int _alphanum(char c)
 {
-
-	for (; c != '\0'; c++)
-	{
-		if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') ||
-			(c >= 'a' && c <= 'z'))
-			return (0);
-	}
-	return (1);
-}*/
+	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
 
 /**
  * *cap_string -  function that capitalizes all words of a string
@@ -24,42 +20,15 @@
  *
  * Return: str
  */
-/*char *cap_string(char *str)
-{
-	int i = 0;
-	int = ((str >= '0' && *str <= '9'), (c >= 'A' && c <= 'Z'), (c >= 'a' && c <= 'z');
-
-	while (str[i])
-	{
-		if ((str[i] >= 'a' && str[i] <= 'z') && (_alphanum(str[i - 1]) || i == 0))
-			str[i] -= 32;
-		i++;
-	}
-	return (str);
-}*/
-int ft_alphanum(char c)
-{
-	if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		return (0);
-	return (1);
-}
-
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		i++;
-	}
-	i = 0;
-	while (str[i])
-	{
-		if ((str[i] >= 'a' && str[i] <= 'z') && (ft_alphanum(str[i - 1]) || i == 0))
-			str[i] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			if (!(_alphanum(str[i - 1]) || i == 0))
+				str[i] -= 32;
 		i++;
 	}
 	return (str);
