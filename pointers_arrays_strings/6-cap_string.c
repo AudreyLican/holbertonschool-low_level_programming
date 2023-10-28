83c8de7eee59a36241ffb90fd38431753db59549
AudreyLican <audrey.lican@gmail.com>
2023-10-28 22:59:36 +0200

    update all code running

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * _alphanum - function that look for letters and numbers
- * @c : char value
- *
- * Return: 0 if alphanum and 1 false
- */
-int _alphanum(char c)
-{
-	if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')
-		|| (c >= 'A' && c <= 'Z'))
-		return (1);
-	return (0);
-}
-
-/**
  * *cap_string -  function that capitalizes all words of a string
  * @str: string
  *
@@ -24,28 +10,20 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		i++;
-	}
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[0] -= 32;
 
-/*	while (str[i] != '\0')
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			if (!(_alphanum(str[i - 1]) || i == 0))
-				str[i] -= 32;
+		if (str[i] == 32 || str[i] == 44 || str[i] == 10
+			|| str[i] == 9 || str[i] == 33 || str[i] == 34
+			|| str[i] == 40 || str[i] == 41 || str[i] == 46
+			|| str[i] == 59 || str[i] == 123 || str[i] == 125)
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
+			}
 		i++;
-	}
-	return (str);
-}
-*/
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (!(_alphanum(str[i - 1]) || i == 0))
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-				str[i] -= 32;
 	}
 	return (str);
 }
