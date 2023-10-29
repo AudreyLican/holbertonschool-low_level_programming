28c7ba71ff71af066435b9a8293153dd62a1af53
AudreyLican <audrey.lican@gmail.com>
2023-10-29 11:19:03 +0100

    create ex 7-leet, code running

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,25 +1,36 @@
 #include "main.h"
 
 /**
-* *leet - function that encodes a string into 1337
-* @str: string
+* *leet - function that encodes a string into 1337.
+* letters a and A are replaced by 4
+* letters e and E are replaced by 3
+* letters o and O are replaced by 0
+* letters t and T are replaced by 7
+* letters l and L are replaced by 1
+* @s: string
 *
 * Return: string encoded
 */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	int	i = 0;
-	int	j = 0;
-	char	c[] = {'a', 'e' , 'o', 't', 'l'};
-	char	d[] = {'4', '3', '0', '7', '1'};
+	int	scount, leetcount; 
+	char	letter[] = "aAeEoOtTlL";
+	char	nums[] = "443300711";
 
-	while (str[i] != '\0')
+	scount = 0; /* scan throught string */
+	while (s[scount] != '\0') /* check if letter is found */
 	{
-		while (j <)
-		if (str[i] == c || str[i] == d)
+		leetcount = 0; /*leet count*/
+		while (leetcount < 10)
 		{
-			str[i] = ;
+			if (letter[leetcount] == s[scount])
+			{
+				s[scount] = nums[leetcount];
+			}
+			leetcount++;
 		}
+		scount++;
 	}
+	return (s);
 }
