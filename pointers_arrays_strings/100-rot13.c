e8e6b9a6119cddcd1693af4850c04365ba7f42e4
AudreyLican <audrey.lican@gmail.com>
2023-10-29 22:44:08 +0100

    create ex 100-rot13, running

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -2,12 +2,29 @@
 
 /**
  * *rot13 - function that encodes a string using
- * @str
- * 
- * 
+ * @s : string
+ *
+ * Return: s
 */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
+	int	i = 0;
 
+	while (s[i])
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
+			{
+				s[i] -= 13;
+				break;
+			}
+			else
+				s[i] += 13;
+				break;
+		}
+		i++;
+	}
+	return (s);
 }
