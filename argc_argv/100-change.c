cdc96429cf7eb0461c0b33282d0248d22050f655
AudreyLican <audrey.lican@gmail.com>
2023-11-03 22:27:10 +0100

    code fixed, loop change and put on each "if" for each condition, running

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -13,44 +13,50 @@
 
 int	main(int argc, char **argv)
 {
-	int	change, cents;
-
-	if (argc < 2)
+	int	cents = atoi(argv[1]);
+	int	change = 0;
+	
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-
 	if (cents < 0)
 	{
 		printf("0\n");
-		return (1);
+		return (0);
 	}
 
-	for (change = 0; cents > 0; change++)
+	while (cents > 0)
 	{
 		if (cents >= 25)
 		{
 			cents -= 25;
+			change++;
 		}
-		else if (cents <= 10)
+		else if (cents >= 10)
 		{
 			cents -= 10;
+			change++;
 		}
-		else if (cents <= 5)
+		else if (cents >= 5)
 		{
 			cents -= 5;
+			change++;
 		}
-		else if (cents <= 2)
+		else if (cents >= 2)
 		{
-			cents -= 12;
+			cents -= 2;
+			change++;
 		}
-		else if (cents <= 1)
+		else
 		{
 			cents -= 1;
+			change++;
 		}
 	}
+
 	printf("%d\n", change);
 	return (0);
 }
+
