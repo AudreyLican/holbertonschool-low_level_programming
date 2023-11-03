b5bd97c7cec22a8ed2e1dfe1d3e2c2acbe9e203c
AudreyLican <audrey.lican@gmail.com>
2023-11-03 22:42:18 +0100

    code fixed, betty style fixed, code reduiced, loop change for a "for loop", running

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -13,50 +13,36 @@
 
 int	main(int argc, char **argv)
 {
-	int	cents = atoi(argv[1]);
-	int	change = 0;
-	
+	int	cents, change;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
+
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	while (cents > 0)
+	for (change = 0; cents > 0; change++)
 	{
 		if (cents >= 25)
-		{
 			cents -= 25;
-			change++;
-		}
 		else if (cents >= 10)
-		{
 			cents -= 10;
-			change++;
-		}
 		else if (cents >= 5)
-		{
 			cents -= 5;
-			change++;
-		}
 		else if (cents >= 2)
-		{
 			cents -= 2;
-			change++;
-		}
 		else
-		{
 			cents -= 1;
-			change++;
-		}
 	}
 
 	printf("%d\n", change);
 	return (0);
 }
-
