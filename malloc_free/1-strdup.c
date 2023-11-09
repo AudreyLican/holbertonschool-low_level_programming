175e3e5678c02bd94c41a5805ab8f1cf02fac459
AudreyLican <audrey.lican@gmail.com>
2023-11-09 11:15:46 +0100

    betty, update return description

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -5,7 +5,7 @@
  * in memory which contains a copy of the string given as a parameter
  * @str : pointer to the sting to copy
  *
- * Return : copy of the string given as parameter
+ * Return: copy of the string given as parameter
  */
 
 char *_strdup(char *str)
