8511e230055d077a6482316ab93ac75f0d572107
AudreyLican <audrey.lican@gmail.com>
2023-11-10 14:34:30 +0100

    change the place of len of s1 and s2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i]; i++)
 		;
-
-	if (s2 == NULL)
-		return (0);
 	for (j = 0; s2[j]; j++)
 		;
 
