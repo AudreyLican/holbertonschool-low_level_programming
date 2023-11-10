c4d28fa721db30c6786078f3ecb039a9d9e57348
AudreyLican <audrey.lican@gmail.com>
2023-11-10 14:43:43 +0100

    withdraw return for empty string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, len1, len2, size;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
