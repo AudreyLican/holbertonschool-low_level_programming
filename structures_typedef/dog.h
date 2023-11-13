5d981e9a89651771b6c7b420a3b099fba46f48b6
AudreyLican <audrey.lican@gmail.com>
2023-11-13 01:03:32 +0100

    create ex0 and readme

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+* struct dog - dogs information
+* @name : name of dog
+* @age : age of dog
+* @owner : dog master
+*/
+
+struct	dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+}
+
+#endif
