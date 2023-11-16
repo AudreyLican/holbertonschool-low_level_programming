ceb5fc88dcb3cca532eea59cb6c218036bde2587
AudreyLican <audrey.lican@gmail.com>
2023-11-16 10:27:09 +0100

    add condition for NULL

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * init_dog - function that initialize a variable of type struct dog
@@ -12,6 +13,8 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	if (d != NULL)
 	{
 		d->name = name;
