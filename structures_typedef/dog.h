2d7f0bd59295b19ca2ca201b8bd4f794bbc8536f
AudreyLican <audrey.lican@gmail.com>
2023-11-16 10:29:04 +0100

    update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
 * struct dog - dogs information
@@ -8,11 +8,21 @@
 * @owner : dog master
 */
 
-struct	dog
+struct dog
 {
 	char	*name;
 	float	age;
 	char	*owner;
-}
+};
+
+/**
+* my_dog - Typedef for dog struct
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
