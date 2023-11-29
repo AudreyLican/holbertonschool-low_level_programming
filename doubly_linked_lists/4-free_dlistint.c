9a7633b738b2912affaf5eb49270d80ddcf63bee
AudreyLican <audrey.lican@gmail.com>
2023-11-29 16:28:07 +0100

    create ex 4-free_dlistint

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the end
+ * of a list_t list.
+ * @head : double pointer to the list_t list
+ * @n : new element to add
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head)
+	{
+		dlistint_t	*temp = head->next;
+
+		free(head);
+		head = temp;
+	}
+}
