1c2ed444c7280743c5e4eca86154df42cbd295d2
AudreyLican <audrey.lican@gmail.com>
2023-11-29 12:00:00 +0100

    change data type for counter, for unsigned int

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -4,25 +4,21 @@
  * list_len - function that returns the number of elements
  * in a linked list_t list.
  * @h : pointer to the list
- * 
+ *
  * Return: the number of list printed
 */
 size_t list_len(const list_t *h)
 {
-	size_t	counter;
-	char	*listptr;
+	unsigned int	counter = 0;
 
-	listptr = h->str;
-
-	counter = 0;
 	while (h)
 	{
-		if (listptr == NULL)
+		if (h->str == NULL)
 		{
 			return (0);
 		}
-		counter++;
 		h = h->next;
+		counter++;
 	}
 	return (counter);
 }
