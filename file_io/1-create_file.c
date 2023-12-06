29095687860d83b697b79a47919523940c584561
AudreyLican <audrey.lican@gmail.com>
2023-12-06 13:14:14 +0100

    add permissions on var. new_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,30 +3,32 @@
 /**
  * create_file - function that creates a file
  * @filename : pointer on the name of the file to create
- * @text-content : pointer to a string to write in a file
- * 
+ * @text_content : pointer to a string to write in a file
+ *
  * Return: 1 on success, otherwise -1 for failure
 */
 int create_file(const char *filename, char *text_content)
 {
-	int	o, w, len;
+	int	new_file, w, len;
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
 		;
 	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	new_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (new_file == -1)
 		return (-1);
-
-	close(o);
+	w = write(new_file, text_content, len);
+	if (w == -1)
+	{
+		close(new_file);
+		return (-1);
+	}
+	close(new_file);
 
 	return (1);
 }
