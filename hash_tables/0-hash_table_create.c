28980fe1a7c031ee1ec7aa2650ab21bb27ad648e
AudreyLican <audrey.lican@gmail.com>
2023-12-09 01:10:53 +0100

    code fixed, return (NULL) misplaced

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,17 +10,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t	*hash;
 
-	if (size)
+	if (size != 0)
 	{
 		hash = malloc(sizeof(hash_table_t));
+
 		if (!hash)
 			return (NULL);
+
 		hash->size = size;
-		hash->array = calloc(size, sizeof(hash_node_t));
+		hash->array = calloc(size, sizeof(hash_node_t *));
 		if (!hash->array)
 			return (NULL);
 		return (hash);
-
-		return (NULL);
 	}
+		return (NULL);
 }
